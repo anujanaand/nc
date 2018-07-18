5231dff051413d009e98142e97106d636509b2e6
aujanand <anand.anuj91@gmail.com>
2018-07-18 15:30:16 +0530

    initial commit

---
+++ src/main/java/anuj6/anand6/App.java
@@ -0,0 +1,13 @@
+package anuj6.anand6;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
